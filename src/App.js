5624986e2daddfeb6de61c0a47d14c390a5a984f
Erik Fazio <fazioerik@gmail.com>
2020-07-30 10:09:23 +0200

    Fix on PokemonCard

--- src/App.js
+++ src/App.js
@@ -4,15 +4,34 @@ import PokemonCard from './components/PokemonCard';
 
 function App() {
     const [pokemon, setPokemon] = useState({});
+    const [input, setInput] = useState('');
 
     useEffect(() => {
-        const apiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto';
+        const apiUrl = `https://pokeapi.co/api/v2/pokemon/${}`;
         axios.get(apiUrl).then((result) => {
             setPokemon(result.data);
         });
     }, [pokemon]);
 
-    return <PokemonCard pokemon={pokemon} />;
+    function handleClick(event) {
+        setInput(event.target.value);
+    }
+
+    return (
+        <div className="container mx-auto">
+            <div className="text-center mt-4">
+                <input className="border rounded mr-2 p-2" type="text" />
+                <button
+                    className="bg-gray-400 p-2 rounded-lg"
+                    type="button"
+                    onClick={handleClick}
+                    value={input}>
+                    Search
+                </button>
+            </div>
+            <PokemonCard pokemon={pokemon} />
+        </div>
+    );
 }
 
 export default App;
