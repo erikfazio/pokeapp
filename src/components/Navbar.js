8e8be418d0c010e793d35ee16bd70d421c191c89
Erik Fazio <fazioerik@gmail.com>
2020-07-30 10:50:56 +0200

    Added Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-red-600 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <span className="font-semibold text-xl tracking-tight">Pokeapp</span>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
