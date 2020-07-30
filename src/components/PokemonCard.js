357a05f4a2e2daef17197191700da97c1d619b26
Erik Fazio <fazioerik@gmail.com>
2020-07-30 15:16:00 +0200

    Added stats chart

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import PokemonType from './PokemonType';
+import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar } from 'recharts';
 
 function PokemonCard({ pokemon }) {
+    const data = pokemon.stats.map((stat) => {
+        return {
+            subject: stat.stat.name.toUpperCase(),
+            A: stat.base_stat,
+            B: 30,
+            fullMark: 150
+        };
+    });
+
     return (
-        <div className="md:bg-gray-200 p-6 rounded">
-            <div className="md:inline-block bg-white shadow-lg rounded-lg  p-6 text-center">
+        <div className="md:flex justify-around md:bg-gray-200 p-6 rounded">
+            <div className="bg-white shadow-lg rounded-lg  p-6 text-center">
                 <span className="text-6xl font-bold text-gray-400">#{pokemon.order}</span>
                 <img className="mx-auto" src={pokemon.sprites.front_default} />
                 <span className="font-bold uppercase">{pokemon.name}</span>
@@ -26,6 +36,13 @@ function PokemonCard({ pokemon }) {
                         {pokemon.abilities.map((ability) => ability.ability.name).join(', ')}
                     </span>
                 </div>
+            </div>
+            <div className="bg-white shadow-lg rounded-lg  p-6 text-center">
+                <RadarChart outerRadius={90} width={700} height={400} data={data}>
+                    <PolarGrid />
+                    <PolarAngleAxis dataKey="subject" />
+                    <Radar name="Pikachu" dataKey="A" stroke="red" fill="red" fillOpacity={0.6} />
+                </RadarChart>
             </div>
         </div>
     );
