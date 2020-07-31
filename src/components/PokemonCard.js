fa69b992d95f51b8f28deb7a4ada2c6f45e09e53
Erik Fazio <fazioerik@gmail.com>
2020-07-31 15:51:22 +0200

    Added PokemonInfo component

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PokemonType from './PokemonType';
+import PokemonInfo from './PokemonInfo';
 import PokemonStatsChart from './PokemonStatsChart';
 
 function PokemonCard({ pokemon }) {
@@ -14,30 +14,7 @@ function PokemonCard({ pokemon }) {
 
     return (
         <div className="container flex flex-col lg:flex-row p-6 justify-between text-center">
-            <div className="p-6 bg-white shadow-lg mr-6 rounded-lg text-center">
-                <span className="text-6xl font-bold text-gray-400">#{pokemon.order}</span>
-                <img className="mx-auto" src={pokemon.sprites.front_default} />
-                <span className="font-bold uppercase">{pokemon.name}</span>
-                <div className="p-4">
-                    <span>
-                        <span className="font-bold">Weight:</span> {pokemon.weight / 10} kg,{' '}
-                        <span className="font-bold">Height: </span> {pokemon.height / 10} m
-                    </span>
-                </div>
-                <div className="p-4">
-                    <div className="flex capitalize justify-center">
-                        {pokemon.types.map((type) => (
-                            <PokemonType type={type.type.name} />
-                        ))}
-                    </div>
-                </div>
-                <div className="p-4">
-                    <span className="capitalize">
-                        <span className="font-bold">Moves: </span>{' '}
-                        {pokemon.abilities.map((ability) => ability.ability.name).join(', ')}
-                    </span>
-                </div>
-            </div>
+            <PokemonInfo pokemon={pokemon} />
             <PokemonStatsChart data={data} />
         </div>
     );
---
+++ src/components/PokemonInfo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PokemonType from './PokemonType';
+
+function PokemonInfo({ pokemon }) {
+    return (
+        <div className="p-6 bg-white shadow-lg mr-6 rounded-lg text-center">
+            <span className="text-6xl font-bold text-gray-400">#{pokemon.order}</span>
+            <img className="mx-auto" src={pokemon.sprites.front_default} />
+            <span className="font-bold uppercase">{pokemon.name}</span>
+            <div className="p-4">
+                <span>
+                    <span className="font-bold">Weight:</span> {pokemon.weight / 10} kg,{' '}
+                    <span className="font-bold">Height: </span> {pokemon.height / 10} m
+                </span>
+            </div>
+            <div className="p-4">
+                <div className="flex capitalize justify-center">
+                    {pokemon.types.map((type) => (
+                        <PokemonType type={type.type.name} />
+                    ))}
+                </div>
+            </div>
+            <div className="p-4">
+                <span className="capitalize">
+                    <span className="font-bold">Moves: </span>{' '}
+                    {pokemon.abilities.map((ability) => ability.ability.name).join(', ')}
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default PokemonInfo;
